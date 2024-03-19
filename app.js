5968e8bee3b2eaa97de76c0d53df3820ab2550b3
venukotamraju <kotamraju.venugopal@gmail.com>
2024-03-19 16:21:56 +0530

    cors issue fix 1

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.URLS,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
